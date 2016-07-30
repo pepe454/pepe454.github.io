971e7b70143f7eea6ce3e84a930609cd63dd950e
e12real <eric@hilhorst.us>
2016-07-29 22:45:06 -0400

    Create email.php

---
+++ email.php
@@ -0,0 +1,19 @@
+---
+layout: null
+---
+<!DOCTYPE html
+<html>
+<head>
+  <title>Emails man</title>
+    {% include head.html %}
+</head>
+<body>
+  <p> Hello World. </p>
+  
+  <p>test PHP:</p>
+  <?php
+  print 2+3;
+  ?>
+  
+</body>
+</html>
